6992869077734c301a9d3c11800c91340eda38b8
김성빈 <20190207@office.kumoh.ac.kr>
2023-09-12 11:41:12 +0900

    test

---
+++ W02Counter/app/src/main/java/com/example/w02counter/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.w02counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.w02counter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var main: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+        main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main.root)
+
+        main.btnAdd.setOnClickListener {
+            main.btnAdd.text = "눌렸습니다."
+            main.txtCount.text = "눌렸습니다."
+        }
+
+    }
+}

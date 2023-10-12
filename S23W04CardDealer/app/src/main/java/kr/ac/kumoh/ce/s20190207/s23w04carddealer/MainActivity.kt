899e964008f03857fd0caa5026b0ccf6c176014b
seongbin <catsb21@gmail.com>
2023-10-12 14:41:29 +0900

    4-5 - 랜덤 카드(미완)

--- S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190207/s23w04carddealer/MainActivity.kt
+++ S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190207/s23w04carddealer/MainActivity.kt
@@ -1,11 +1,13 @@
 package kr.ac.kumoh.ce.s20190207.s23w04carddealer
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.ac.kumoh.ce.s20190207.s23w04carddealer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var main: ActivityMainBinding;
+    @SuppressLint("DiscouragedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
@@ -13,7 +15,30 @@ class MainActivity : AppCompatActivity() {
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
-        main.card1.setImageResource(R.drawable.c_2_of_hearts)
-    }
+        //main.card1.setImageResource(R.drawable.c_2_of_hearts)
 
+        val res = resources.getIdentifier(
+            // 하드 코딩 제거할 것
+            getCardName(37), "drawable", packageName
+            )
+        main.card1.setImageResource(res)
+    }
+    private fun getCardName(c: Int) : String{
+        val shape = when(c / 13){
+            0 -> "spades"
+            1 -> "diamonds"
+            2 -> "hearts"
+            3 -> "clubs"
+            else -> "error"
+        }
+        val number = when (c % 13){
+            0 -> "ace"
+            in 1..9 -> (c % 13 + 1).toString()
+            10 -> "jack"
+            11 -> "queen"
+            12 -> "king"
+            else -> "error"
+        }
+        return "c_${number}_of_${shape}"
+    }
 }

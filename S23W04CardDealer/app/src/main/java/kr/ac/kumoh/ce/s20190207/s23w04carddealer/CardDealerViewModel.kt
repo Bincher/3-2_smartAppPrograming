b2769a9bedfe733876670b84930042a0469229e3
seongbin <catsb21@gmail.com>
2023-11-07 13:54:05 +0900

    5 - 포커 족보 과제(로직 완, 통계 작성중)

--- S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190207/s23w04carddealer/CardDealerViewModel.kt
+++ S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190207/s23w04carddealer/CardDealerViewModel.kt
@@ -23,4 +23,40 @@ class CardDealerViewModel : ViewModel(){
         _cards.value = newCards
     }
 
+    fun checkPokerHand(cards: IntArray): String {
+        val cardCount = IntArray(13) { 0 }
+        val suits = IntArray(4) { 0 }
+
+        // 카드 숫자와 모양 수를 카운트
+        for (card in cards) {
+            val number = card % 13
+            val suit = card / 13
+            cardCount[number]++
+            suits[suit]++
+        }
+
+        // 족보 판별 로직
+        val isFlush = suits.any { it >= 5 }
+        val isStraight = (0 until 9).any { i ->
+            cardCount.slice(i until i + 5).all { it >= 1 }
+        }
+        val isFourOfAKind = cardCount.any { it >= 4 }
+        val isThreeOfAKind = cardCount.any { it >= 3 }
+        val isOnePair = cardCount.count { it == 2 } == 1
+        val isTwoPair = cardCount.count { it == 2 } == 2
+
+        // 족보 판별 및 결과 반환
+        return when {
+            isFlush && isStraight -> "스트레이트 플러쉬"
+            isFourOfAKind -> "포카드"
+            isThreeOfAKind && isOnePair -> "풀하우스"
+            isFlush -> "플러쉬"
+            isStraight -> "스트레이트"
+            isThreeOfAKind -> "트리플"
+            isTwoPair -> "투페어"
+            isOnePair -> "원페어"
+            else -> "탑"
+        }
+    }
+
 }
--- S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190207/s23w04carddealer/MainActivity.kt
+++ S23W04CardDealer/app/src/main/java/kr/ac/kumoh/ce/s20190207/s23w04carddealer/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.ac.kumoh.ce.s20190207.s23w04carddealer
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kr.ac.kumoh.ce.s20190207.s23w04carddealer.databinding.ActivityMainBinding
@@ -23,23 +24,52 @@ class MainActivity : AppCompatActivity() {
             val res = IntArray(5)
             for (i in res.indices) {
                 res[i] = resources.getIdentifier(
-                getCardName(it[i]), "drawable", packageName
+                    getCardName(it[i]), "drawable", packageName
                 )
             }
 
-            main.card1.setImageResource(res[0])
-            //main.card2.setImageResource(res[1])
-            //main.card3.setImageResource(res[2])
-            //main.card4.setImageResource(res[3])
-            //main.card5.setImageResource(res[4])
-        }
+            val cardViews = arrayOf(main.card1, main.card2, main.card3, main.card4, main.card5)
+
+            for (i in cardViews.indices) {
+                cardViews[i].setImageResource(res[i])
+            }
+
+            }
         )
 
         main.btnShuffle.setOnClickListener {
-            model.shuffle()
+            shuffleAndHand()
+        }
+        main.btn1000.setOnClickListener {
+            for(i in 1..1000){
+                shuffleAndHand()
+            }
+        }
+        main.btnFlush.setOnClickListener{
+            shuffleAndHand()
+            while(main.textView1.text != "플러쉬"
+                && main.textView1.text != "풀하우스"
+                && main.textView1.text != "포카드"
+                && main.textView1.text != "스트레이트 플러쉬"){
+                shuffleAndHand()
+            }
+        }
+        main.btnFourcard.setOnClickListener{
+            shuffleAndHand()
+            while(main.textView1.text != "포카드"
+                && main.textView1.text != "스트레이트 플러쉬"){
+                shuffleAndHand()
+            }
+        }
+        main.textView1.setOnClickListener {
+            Toast.makeText(this, "시험용", Toast.LENGTH_SHORT).show()
         }
     }
-
+    private fun shuffleAndHand(){
+        model.shuffle()
+        val cards = model.cards.value ?: intArrayOf() // 현재 카드 배열
+        main.textView1.text = model.checkPokerHand(cards) // 포커 족보 판별
+    }
     private fun getCardName(c: Int) : String{
         var shape = when(c / 13){
             0 -> "spades"

d20f0dbb20681be9cd2bb82e9156d96dc17b476a
seongbin <catsb21@gmail.com>
2023-12-10 00:27:42 +0900

    💄 기말플젝 - AsyncImage 및 Card 사용

--- FinalProject/app/src/main/java/kr/ac/kumoh/ce/s20190207/finalproject/Cookie.kt
+++ FinalProject/app/src/main/java/kr/ac/kumoh/ce/s20190207/finalproject/Cookie.kt
@@ -4,7 +4,7 @@ data class Cookie(
     val id: Int,
     val name: String,
     val img: String,
-    val skill: String,
+    val skill: String?,
     val element: String,
     val Location_name: String,
     val Type_name: String,

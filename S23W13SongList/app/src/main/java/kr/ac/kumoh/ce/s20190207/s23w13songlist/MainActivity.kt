8cc63d45503c6cfa991854a0d47c19b1a9197065
seongbin <catsb21@gmail.com>
2023-12-09 10:13:27 +0900

    🍱 세부 화면 추가

--- S23W13SongList/app/src/main/java/kr/ac/kumoh/ce/s20190207/s23w13songlist/MainActivity.kt
+++ S23W13SongList/app/src/main/java/kr/ac/kumoh/ce/s20190207/s23w13songlist/MainActivity.kt
@@ -48,13 +48,11 @@ import coil.compose.AsyncImage
 import kr.ac.kumoh.ce.s20190207.s23w13songlist.ui.theme.S23W13SongListTheme
 
 
-private val songs = mutableListOf<Song>()
-
 class MainActivity : ComponentActivity() {
+    private val viewModel: SongViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel: SongViewModel by viewModels()
-
         setContent {
             MainScreen(viewModel)
         }
@@ -64,78 +62,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(viewModel: SongViewModel) {
     val songList by viewModel.songList.observeAsState(emptyList())
+
     S23W13SongListTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            MyList(songList)
+            SongApp(songList)
         }
     }
 }
-
-@Composable
-fun MyList(list: List<Song>) {
-    LazyColumn(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(horizontal = 8.dp)
-    ) {
-        itemsIndexed(list)  { index, song ->
-            SongItem(index, song)
-        }
-    }
-}
-
-@Composable
-fun SongItem(index: Int, song: Song) {
-    var expanded by remember {mutableStateOf(false)}
-    Card(
-        elevation = CardDefaults.cardElevation(8.dp),
-        modifier = Modifier.clickable { expanded = !expanded }
-    ){
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(IntrinsicSize.Min)
-                .background(Color(255, 255, 255))
-                .padding(8.dp)
-        ){
-            AsyncImage(
-                model = "https://picsum.photos/300/300?random=${index}",
-                contentDescription = "노래 앨범 사진",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(100.dp)
-                    .clip(RoundedCornerShape(10))
-//                .clip(CircleShape)
-            )
-            Spacer(
-                modifier = Modifier.width(10.dp)
-            )
-            Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.SpaceAround
-            ) {
-                TextTitle(song.title)
-                TextSinger("가수 : ${song.singer}")
-            }
-        }
-        AnimatedVisibility(visible = expanded) {
-            song.lyrics?.let{Text(it)}
-        }
-    }
-
-
-}
-
-@Composable
-fun TextTitle(title: String) {
-    Text(title, fontSize = 30.sp)
-}
-
-@Composable
-fun TextSinger(singer: String) {
-    Text(singer, fontSize = 20.sp)
-}
-
